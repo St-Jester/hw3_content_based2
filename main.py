8d2c50fe792e6eab167833c20f2934e645c99ea3
stellarjester@gmail.com <gU4&3f/DwxWBkZ&>
2020-12-11 09:01:41 +0200

    task 3

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+
 import numpy as np
 import cv2 as cv
+import time
 import sys
 import zipfile
 
@@ -86,5 +89,66 @@ def read_and_plot(img_name, plot=False, test=False):
 
 # print(distance(v1, v2))
 # print(distance(v1, v1_2))
-read_and_plot('../Homework1/ukbench00004.jpg')
+# read_and_plot('../Homework1/ukbench00004.jpg')
 image_quieries = ['ukbench00004.jpg', 'ukbench00040.jpg', 'ukbench00060.jpg', 'ukbench00588.jpg', 'ukbench01562.jpg']
+
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv.imread(os.path.join(folder, filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+
+def find_similar(query, images):
+    print('find_similar')
+    histogram_vector_query = get_color_hist_rgb(query)
+
+    all_distances = np.zeros(len(images))
+    for (i, image) in enumerate(images):
+        histogram_vector = get_color_hist_rgb(image)
+        if i % 100 == 0:
+            print(f"done histogram_vector{i}")
+        all_distances[i] = distance(histogram_vector_query, histogram_vector)
+        if i % 100 == 0:
+            print(f"done all_distances{i}")
+
+    return all_distances
+
+
+def find_n_smallest(distances, k=11):
+    print(f"find_n_smallest")
+    return np.argpartition(distances, k)
+
+
+for im_query in image_quieries:
+    start = time.time()
+
+    im = cv.imread('../Homework1/' + im_query)
+    images = load_images_from_folder('../Homework1/')
+    found_distances = find_similar(im, images)
+
+    smallest_indices = find_n_smallest(found_distances)
+    print(smallest_indices[1:11])
+    small_images = []
+    for img_index in smallest_indices[1:11]:
+        small_images.append(cv.resize(images[img_index], (100, 100)))
+
+    numpy_vertical = np.vstack(small_images)
+
+    end = time.time()
+    print(f"time elapsed: {end - start}")
+    cv.imwrite(im_query+'_results.jpg', numpy_vertical)
+
+# # [ 5  9  7 61 60  0  1  3 11 10]
+# window_name = 'image'
+#
+# numpy_vertical = np.vstack((images[5], images[9]))
+# cv.imshow('Numpy Vertical', numpy_vertical)
+# #
+# # cv.imshow(window_name, images[5])
+# # cv.imshow(window_name, images[9])
+cv.waitKey()
+
